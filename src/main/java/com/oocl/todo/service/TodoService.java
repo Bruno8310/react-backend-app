39ff395cb2264f2448a2e14cb06480147c8963ac
userName <gongl3730@gmail.com>
2020-08-06 11:42:36 +0800

    [TodoList][Bruno] feature: add should_return_todolist_when_get_todolist_given_nothing method

--- src/main/java/com/oocl/todo/service/TodoService.java
+++ src/main/java/com/oocl/todo/service/TodoService.java
@@ -1,13 +1,22 @@
 package com.oocl.todo.service;
 
+import com.oocl.todo.model.Todo;
 import com.oocl.todo.repository.TodoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TodoService {
+
     private final TodoRepository todoRepository;
 
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
+    }
+
+
+    public List<Todo> getTodoList() {
+        return null;
     }
 }
