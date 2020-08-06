dea0cdbbcb976d02c23bf770e2cd3834055a8de5
userName <gongl3730@gmail.com>
2020-08-06 11:20:29 +0800

    [TodoList][Bruno] feature: add FlyWay to control h2 database

--- src/main/java/com/oocl/todo/controller/TodoController.java
+++ src/main/java/com/oocl/todo/controller/TodoController.java
@@ -1,13 +1,17 @@
 package com.oocl.todo.controller;
 
 import com.oocl.todo.service.TodoService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/todos")
 public class TodoController {
+
     private final TodoService todoService;
 
     public TodoController(TodoService todoService) {
         this.todoService = todoService;
     }
+
 }
--- src/main/java/com/oocl/todo/model/Todo.java
+++ src/main/java/com/oocl/todo/model/Todo.java
@@ -1,8 +1,20 @@
 package com.oocl.todo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String content;
+
     private Boolean status;
 
     public Todo() {
@@ -30,5 +42,20 @@ public class Todo {
 
     public void setStatus(Boolean status) {
         this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Todo todo = (Todo) o;
+        return Objects.equals(id, todo.id) &&
+                Objects.equals(content, todo.content) &&
+                Objects.equals(status, todo.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content, status);
     }
 }
